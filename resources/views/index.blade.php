dbd265efdd49f88383673c761410e535d73a9f61
Michael Price <mikkyx@gmail.com>
2016-10-13 23:49:23 +0100

    redo index blade to extend master

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,10 +1,18 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <title>Tweet It Live - The best app for tweeting your event live</title>
-</head>
-<body>
-    <a href="/twitter/login">Sign in with Twitter</a>
-</body>
-</html>
+@extends('master')
+@section('meta-title','Tweet It Live - The best app for tweeting from your event')
+@section('body-id','homepage')
+
+@section('content')
+    <div id="start-here">
+        <p>Start here...</p>
+        <p>
+            <a class="button is-twitter is-large" href="/twitter/login">
+                <span class="icon is-medium">
+                    <i class="fa fa-twitter"></i>
+                </span>
+                <span>Sign in with Twitter</span>
+            </a>
+        </p>
+        <p><a href="/about">About</a> | <a href="/privacy">Privacy Policy</a> | <a href="/terms">T&amp;Cs</a></p>
+    </div>
+@endsection
