67a8940ef53e18e1f26fb214ed75e977d9cafc67
Michael Price <mikkyx@gmail.com>
2016-10-13 23:49:42 +0100

    overhaul of tweet form

--- resources/views/form.blade.php
+++ resources/views/form.blade.php
@@ -1,34 +1,7 @@
-<!doctype html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link href="https://cdnjs.cloudflare.com/ajax/libs/bulma/0.2.1/css/bulma.min.css" rel="stylesheet" type="text/css" />
-    <title>Tweet It Live - Compose</title>
-    @if (env('APP_ENV') == 'local')
-    <script src="/js/vue.js"></script>
-    @else
-
-    @endif
-    <style type="text/css">
-        h4.title {
-            margin-bottom: 2px;
-        }
-
-        .length-ok {
-            color: #00cc00;
-        }
-
-        .over-limit {
-            color: #cc0000;
-            font-weight: bold;
-        }
-    </style>
-</head>
-<body>
-    <?php print_r(session('twitterUser')) ?>
+@extends('master')
+@section('meta-title','@'.session('twitterUser')->nickname.' - Tweet It Live')
+@section('body-id','form')
+@section('content')
     <div class="columns">
         <div class="column is-6 is-offset-3">
             <div class="columns">
@@ -38,28 +11,40 @@
                     </figure>
                 </div>
                 <div class="column">
-                    <form action="/post" enctype="multipart/form-data" method="post">
+                    <form action="/tweet" method="post" v-on:submit.prevent="postTweet">
                         {!! csrf_field() !!}
-                        <h4 class="title is-4" style="margin-bottom: 5px">{{ '@'.session('twitterUser')->nickname }}: <span class="is-pulled-right" v-bind:class="lengthTest">@{{ remaining_characters }}</span></h4>
-                        <textarea class="textarea" cols="40" id="tweet" name="tweet" rows="4" v-model="tweet"></textarea><br />
-                        Suffix tweets with:
-                        <input class="input" id="suffix_hashtags" name="suffix_hashtags" type="text" v-model="suffix_hashtags" /><br />
-                        Image:<br />
-                        <input id="image" name="image" type="file" /><br />
-                        <input class="button is-primary is-pulled-right" id="send_tweet" type="submit" v-bind:disabled="remaining_characters < 0" />
+                        <h4 class="title is-4" style="margin-bottom: 5px">{{ '@'.session('twitterUser')->nickname }} <span class="is-pulled-right" v-bind:class="lengthTest">@{{ remaining_characters }}</span></h4>
+                        <div class="control">
+                            <textarea class="textarea" cols="40" id="tweet" name="tweet" rows="4" v-model="tweet" v-on:keyup.enter.prevent="enterPostTweet"></textarea>
+                            <span class="help"><input id="press_enter" name="press_enter" type="checkbox" v-model="pressEnterToSend" /> <label for="press_enter">Press Enter to Send</label></span>
+                        </div>
+                        <div class="control is-horizontal">
+                            <div class="control-label">
+                                <label class="label">
+                                    Suffix:
+                                </label>
+                            </div>
+                            <div class="control">
+                                <input class="input" id="suffix_hashtags" name="suffix_hashtags" type="text" v-model="suffixHashtags" />
+                            </div>
+                        </div>
+                        <input class="button is-twitter is-pulled-right" id="send_tweet" type="submit" v-bind:disabled="remaining_characters == 139 || remaining_characters < 0" />
                     </form>
                 </div>
             </div>
         </div>
     </div>
-
+@endsection
+@push('scripts')
+    <script src="https://cdn.jsdelivr.net/vue.resource/1.0.3/vue-resource.min.js"></script>
     <script>
         new Vue({
             el: 'body',
             data: {
+                pressEnterToSend: true,
+                suffixHashtags: '',
                 tweet: '',
-                suffix_hashtags: '',
-                tweet_max_length: 140
+                tweetMaxLength: 140
             },
             computed: {
                 lengthTest: function() {
@@ -70,10 +55,30 @@
                 },
                 remaining_characters: function() {
                     // Return allowed length, minus length of tweet, length of suffix, and 1 for space
-                    return this.tweet_max_length - this.tweet.length - this.suffix_hashtags.length - 1
+                    return this.tweetMaxLength - this.tweet.length - this.suffixHashtags.length - 1
+                }
+            },
+            methods: {
+                enterPostTweet: function() {
+                    if (this.pressEnterToSend) {
+                        this.postTweet();
+                    }
+                },
+                postTweet: function() {
+                    this.$http.post('/tweet',{
+                        '_token': document.querySelector('input[name=_token]').value,
+                        'tweet': document.getElementById('tweet').value,
+                        'suffix_hashtags': document.getElementById('suffix_hashtags').value
+                    }).then((response) => {
+                        if (response.body == 'OK') {
+                            // Tweet sent successfully!
+                            // Reset the form for the next one
+                            this.tweet = '';
+                            document.getElementById('tweet').focus();
+                        }
+                    });
                 }
             }
         });
     </script>
-</body>
-</html>
+@endpush
