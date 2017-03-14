9e7b13695c1bad03af71de265f646079ba67dfc1
Michael Price <mikkyx@gmail.com>
2017-03-14 23:07:50 +0000

    refactor default header into a partial

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -2,17 +2,7 @@
 @section('meta-title','Tweet It Live - The best app for tweeting from your event')
 @section('body-id','homepage')
 @section('header')
-    <div class="col-xs-12 col-md-6">
-        <img src="/images/logo.svg" title="envivo social" />
-    </div>
-    <div class="col-xs-12 col-md-6 headerLinks text-center">
-        <a href="/about">About</a>
-        <a href="/privacy">Privacy</a>
-        <a href="/terms">Terms</a>
-        <a class="btn btn-info hidden-xs" href="/twitter/login">
-            <i class="fa fa-twitter"></i>&nbsp;&nbsp;Sign in with Twitter
-        </a>
-    </div>
+    @include('partials.default-header')
 @endsection
 @section('content')
         <div class="row">
---
+++ resources/views/partials/default-header.blade.php
@@ -0,0 +1,12 @@
+
+<div class="col-xs-12 col-md-6">
+    <img src="/images/logo.svg" title="envivo social" />
+</div>
+<div class="col-xs-12 col-md-6 headerLinks text-center">
+    <a href="/about">About</a>
+    <a href="/privacy">Privacy</a>
+    <a href="/terms">Terms</a>
+    <a class="btn btn-info hidden-xs" href="/twitter/login">
+        <i class="fa fa-twitter"></i>&nbsp;&nbsp;Sign in with Twitter
+    </a>
+</div>
