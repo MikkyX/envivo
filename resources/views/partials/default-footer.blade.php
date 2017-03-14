e5dc6afd1762d406fa9a5f19b8a91606c9682f4e
Michael Price <mikkyx@gmail.com>
2017-03-14 23:10:16 +0000

    refactor default footer into a partial

---
+++ resources/views/partials/default-footer.blade.php
@@ -0,0 +1,16 @@
+
+<footer>
+    <div class="container">
+        <div class="row">
+            <div class="col-xs-12 col-sm-9">
+                <p>v{{ config('release.version') }} - Made with love, Laravel and Vue.js by <a href="https://www.mikkyx.co.uk">Michael Price</a></p>
+            </div>
+            <div class="col-xs-12 col-sm-3">
+                <p>
+                    <a href="https://github.com/mikkyx/envivo" title="View the code on GitHub"><i class="fa fa-github fa-2x"></i></a>&nbsp;&nbsp;
+                    <a href="https://twitter.com/envivosocial" title="Tweet us"><i class="fa fa-twitter fa-2x"></i></a>
+                </p>
+            </div>
+        </div>
+    </div>
+</footer>
