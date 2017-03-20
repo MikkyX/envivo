8953310c921968d03925e2620bc48e5fa20e4502
Michael Price <mikkyx@gmail.com>
2017-03-20 21:31:15 +0000

    change homepage meta and intro text

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,5 +1,5 @@
 @extends('master')
-@section('meta-title','Tweet It Live - The best app for tweeting from your event')
+@section('meta-title','Tweet live from your conference, meeting or event, quickly and easily - This is Envivo Social')
 @section('body-id','homepage')
 @section('header')
     @include('partials.default-header')
@@ -9,7 +9,7 @@
             <div class="col-xs-12 text-center" id="homepageContent">
                 <h1>Social. Sped Up.</h1>
                 <p>Envivo Social makes it fast and easy to share live updates from your conference, meeting or event with your followers.</p>
-                <p>Got something to share? What are you waiting for?</p>
+                <p>If you've got something to share, what are you waiting for?</p>
                 <p>
                     <a class="btn btn-lg btn-info" href="/twitter/login">
                         <i class="fa fa-twitter"></i>&nbsp;&nbsp;Sign in with Twitter
